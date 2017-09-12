eda6e3c99d91fa59476a39114df115ae2601aeaa
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-09-13 01:04:01 +0200

    gopractice: 02 foundation. Flags PrintDefaults for help

--- 02foundation/flag_cli.go
+++ 02foundation/flag_cli.go
@@ -7,17 +7,26 @@ import (
 
 var name = flag.String("name", "World", "A name to say hello to")
 var spanish bool
+var help bool
 
 func init() {
 	flag.BoolVar(&spanish, "spanish", false, "Use Spanish language")
 	flag.BoolVar(&spanish, "s", false, "Use Spanish language")
+	flag.BoolVar(&help, "h", false, "Print this help")
+	flag.BoolVar(&help, "help", false, "Print this help")
 }
 
 func main() {
 
 	//go run .\02foundation\flag_cli.go -s -name=Mundo
+	//go run .\02foundation\flag_cli.go -h
 
 	flag.Parse()
+
+	if help {
+		flag.PrintDefaults()
+		return
+	}
 
 	if spanish == true {
 		fmt.Printf("Hola %s\n", *name)
